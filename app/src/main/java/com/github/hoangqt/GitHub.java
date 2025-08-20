2396b2750560e1afe896a7947dac85666bb06a4f
Hoang Tran <hoangqt@gmail.com>
2025-08-20 11:37:51 -0400

    Fix formatting

--- app/src/main/java/com/github/hoangqt/GitHub.java
+++ app/src/main/java/com/github/hoangqt/GitHub.java
@@ -1,95 +1,95 @@
 package com.github.hoangqt;
 
+import static io.restassured.RestAssured.given;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
-
 public class GitHub {
-    private static final String GITHUB_API_URL = "https://api.github.com";
+  private static final String GITHUB_API_URL = "https://api.github.com";
 
-    private String token;
-    private String owner;
+  private String token;
+  private String owner;
 
-    static {
-        RestAssured.baseURI = GITHUB_API_URL;
-    }
+  static {
+    RestAssured.baseURI = GITHUB_API_URL;
+  }
 
-    public GitHub(String token, String owner) {
-        this.token = token;
-        this.owner = owner;
-    }
+  public GitHub(String token, String owner) {
+    this.token = token;
+    this.owner = owner;
+  }
 
-    public String getOwner() {
-        return owner;
-    }
+  public String getOwner() {
+    return owner;
+  }
 
-    public String getToken() {
-        return token;
-    }
+  public String getToken() {
+    return token;
+  }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
+  public void setToken(String token) {
+    this.token = token;
+  }
 
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
+  public void setOwner(String owner) {
+    this.owner = owner;
+  }
 
-    public Response getRepository(String repo) {
-        return given()
-                .header("Authorization", "Bearer " + this.token)
-                .header("Accept", "application/vnd.github.v3+json")
-                .pathParam("owner", this.owner)
-                .pathParam("repo", repo)
-                .when()
-                .get("/repos/{owner}/{repo}");
-    }
+  public Response getRepository(String repo) {
+    return given()
+        .header("Authorization", "Bearer " + this.token)
+        .header("Accept", "application/vnd.github.v3+json")
+        .pathParam("owner", this.owner)
+        .pathParam("repo", repo)
+        .when()
+        .get("/repos/{owner}/{repo}");
+  }
 
-    public Response getRepositoryIssues(String repo) {
-        return given()
-                .header("Authorization", "Bearer " + this.token)
-                .header("Accept", "application/vnd.github.v3+json")
-                .pathParam("owner", this.owner)
-                .pathParam("repo", repo)
-                .when()
-                .get("/repos/{owner}/{repo}/issues");
-    }
+  public Response getRepositoryIssues(String repo) {
+    return given()
+        .header("Authorization", "Bearer " + this.token)
+        .header("Accept", "application/vnd.github.v3+json")
+        .pathParam("owner", this.owner)
+        .pathParam("repo", repo)
+        .when()
+        .get("/repos/{owner}/{repo}/issues");
+  }
 
-    public Response getRepositoryCommits(String repo) {
-        return given()
-                .header("Authorization", "Bearer " + this.token)
-                .header("Accept", "application/vnd.github.v3+json")
-                .pathParam("owner", this.owner)
-                .pathParam("repo", repo)
-                .when()
-                .get("/repos/{owner}/{repo}/commits");
-    }
+  public Response getRepositoryCommits(String repo) {
+    return given()
+        .header("Authorization", "Bearer " + this.token)
+        .header("Accept", "application/vnd.github.v3+json")
+        .pathParam("owner", this.owner)
+        .pathParam("repo", repo)
+        .when()
+        .get("/repos/{owner}/{repo}/commits");
+  }
 
-    public Response createIssue(String repo, String body) {
-        return given()
-                .header("Authorization", "Bearer " + this.token)
-                .header("Accept", "application/vnd.github+json")
-                .header("X-GitHub-Api-Version", "2022-11-28")
-                .header("Content-Type", "application/json")
-                .pathParam("owner", this.owner)
-                .pathParam("repo", repo)
-                .body(body)
-                .when()
-                .post("/repos/{owner}/{repo}/issues");
-    }
+  public Response createIssue(String repo, String body) {
+    return given()
+        .header("Authorization", "Bearer " + this.token)
+        .header("Accept", "application/vnd.github+json")
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("Content-Type", "application/json")
+        .pathParam("owner", this.owner)
+        .pathParam("repo", repo)
+        .body(body)
+        .when()
+        .post("/repos/{owner}/{repo}/issues");
+  }
 
-    public Response updateIssue(String repo, String body, String issueNumber) {
-        return given()
-                .header("Authorization", "Bearer " + this.token)
-                .header("Accept", "application/vnd.github+json")
-                .header("X-GitHub-Api-Version", "2022-11-28")
-                .header("Content-Type", "application/json")
-                .pathParam("owner", this.owner)
-                .pathParam("repo", repo)
-                .pathParam("issueNumber", issueNumber)
-                .body(body)
-                .when()
-                .patch("/repos/{owner}/{repo}/issues/{issueNumber}");
-    }
+  public Response updateIssue(String repo, String body, String issueNumber) {
+    return given()
+        .header("Authorization", "Bearer " + this.token)
+        .header("Accept", "application/vnd.github+json")
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("Content-Type", "application/json")
+        .pathParam("owner", this.owner)
+        .pathParam("repo", repo)
+        .pathParam("issueNumber", issueNumber)
+        .body(body)
+        .when()
+        .patch("/repos/{owner}/{repo}/issues/{issueNumber}");
+  }
 }
--- app/src/main/java/com/github/hoangqt/TestProperties.java
+++ app/src/main/java/com/github/hoangqt/TestProperties.java
@@ -4,57 +4,56 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class TestProperties {
-    private Properties properties;
-    private String token;
-    private String owner;
+  private Properties properties;
+  private String token;
+  private String owner;
 
-    private static final String GITHUB_PAT_KEY = "github-pat";
-    private static final String REPO_OWNER_KEY = "owner";
+  private static final String GITHUB_PAT_KEY = "github-pat";
+  private static final String REPO_OWNER_KEY = "owner";
 
-    private static final String TEST_PROPERTIES_FILE = "src/test/resources/test.properties";
+  private static final String TEST_PROPERTIES_FILE = "src/test/resources/test.properties";
 
-    private TestProperties() {
+  private TestProperties() {}
+
+  public static TestProperties create() {
+    var properties = new TestProperties();
+    properties.loadProperties();
+    properties.parseToken();
+    properties.parseOwner();
+    return properties;
+  }
+
+  private void loadProperties() {
+    properties = new Properties();
+    try (FileInputStream fis = new FileInputStream(TEST_PROPERTIES_FILE)) {
+      properties.load(fis);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to load test.properties", e);
     }
+  }
 
-    public static TestProperties create() {
-        var properties = new TestProperties();
-        properties.loadProperties();
-        properties.parseToken();
-        properties.parseOwner();
-        return properties;
+  private void parseToken() {
+    if (properties.getProperty(GITHUB_PAT_KEY) != null) {
+      token = properties.getProperty(GITHUB_PAT_KEY);
+    } else {
+      String githubToken = System.getenv("GITHUB_PAT");
+      if (githubToken != null && !githubToken.trim().isEmpty()) {
+        token = githubToken;
+      }
     }
+  }
 
-    private void loadProperties() {
-        properties = new Properties();
-        try (FileInputStream fis = new FileInputStream(TEST_PROPERTIES_FILE)) {
-            properties.load(fis);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load test.properties", e);
-        }
+  private void parseOwner() {
+    if (properties.getProperty(REPO_OWNER_KEY) != null) {
+      owner = properties.getProperty(REPO_OWNER_KEY);
     }
+  }
 
-    private void parseToken() {
-        if (properties.getProperty(GITHUB_PAT_KEY) != null) {
-            token = properties.getProperty(GITHUB_PAT_KEY);
-        } else {
-            String githubToken = System.getenv("GITHUB_PAT");
-            if (githubToken != null && !githubToken.trim().isEmpty()) {
-                token = githubToken;
-            }
-        }
-    }
+  public String getToken() {
+    return token;
+  }
 
-    private void parseOwner() {
-        if (properties.getProperty(REPO_OWNER_KEY) != null) {
-            owner = properties.getProperty(REPO_OWNER_KEY);
-        }
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
+  public String getOwner() {
+    return owner;
+  }
 }
